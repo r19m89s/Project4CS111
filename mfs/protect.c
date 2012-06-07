74390104464d39174e8029bdb28a5e420791a33e
Rosie <rstevenson@x2engine.com>
2012-06-07 15:20:00 -0700

    changes to sys call

--- mfs/protect.c
+++ mfs/protect.c
@@ -1,78 +1,208 @@
+/* This file deals with protection in the file system.  It contains the code
+ * for four system calls that relate to protection.
+ *
+ * The entry points into this file are
+ *   do_chmod:	perform the CHMOD and FCHMOD system calls
+ *   do_chown:	perform the CHOWN and FCHOWN system calls
+ *   do_umask:	perform the UMASK system call
+ *   do_access:	perform the ACCESS system call
+ */
+
 #include "fs.h"
-#include "inode.h"
-#include "super.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <minix/callnr.h>
+#include "file.h"
+#include "fproc.h"
+#include "path.h"
+#include "param.h"
 #include <minix/vfsif.h>
+#include "vnode.h"
+#include "vmnt.h"
 
-typedef struct arrayENT{
-    unsigned int userid;
-    unsigned int key1;
-    unsigned int key2;
-}arrayENT;
-arrayENT  ARRAY[8];
-
-FORWARD _PROTOTYPE( int in_group, (gid_t grp)				);
-
-
-/*===========================================================================*
- *				fs_chmod				     *
+/*==================================================================
+ *				do_chmod				     *
  *===========================================================================*/
-PUBLIC int fs_chmod()
+PUBLIC int do_chmod()
 {
-/* Perform the chmod(name, mode) system call. */
+/* Perform the chmod(name, mode) and fchmod(fd, mode) system calls. */
 
-  register struct inode *rip;
-  mode_t mode;
+  struct filp *flp;
+  struct vnode *vp;
+  struct vmnt *vmp;
+  int r;
+  mode_t new_mode;
+  char fullpath[PATH_MAX];
+  struct lookup resolve;
 
-  mode = (mode_t) fs_m_in.REQ_MODE;
-  
-  /* Temporarily open the file. */
-  if( (rip = get_inode(fs_dev, (ino_t) fs_m_in.REQ_INODE_NR)) == NULL)
-	  return(EINVAL);
- 
-  if(rip->i_sp->s_rd_only) {
-  	put_inode(rip);
-	return EROFS;
+  flp = NULL;
+
+  lookup_init(&resolve, fullpath, PATH_NOFLAGS, &vmp, &vp);
+  resolve.l_vmnt_lock = VMNT_WRITE;
+  resolve.l_vnode_lock = VNODE_WRITE;
+
+  if (call_nr == CHMOD) {
+	/* Temporarily open the file */
+	if (fetch_name(m_in.name, m_in.name_length, M3, fullpath) != OK)
+		return(err_code);
+	if ((vp = eat_path(&resolve, fp)) == NULL) return(err_code);
+  } else {	/* call_nr == FCHMOD */
+	/* File is already opened; get a pointer to vnode from filp. */
+	if ((flp = get_filp(m_in.fd, VNODE_WRITE)) == NULL)
+		return(err_code);
+	vp = flp->filp_vno;
+	dup_vnode(vp);
   }
 
-  /* Now make the change. Clear setgid bit if file is not in caller's grp */
-  rip->i_mode = (rip->i_mode & ~ALL_MODES) | (mode & ALL_MODES);
-  rip->i_update |= CTIME;
-  IN_MARKDIRTY(rip);
+  /* Only the owner or the super_user may change the mode of a file.
+   * No one may change the mode of a file on a read-only file system.
+   */
+  if (vp->v_uid != fp->fp_effuid && fp->fp_effuid != SU_UID)
+	r = EPERM;
+  else
+	r = read_only(vp);
 
-  /* Return full new mode to caller. */
-  fs_m_out.RES_MODE = rip->i_mode;
+  if (r == OK) {
+	/* Now make the change. Clear setgid bit if file is not in caller's
+	 * group */
+	if (fp->fp_effuid != SU_UID && vp->v_gid != fp->fp_effgid)
+		m_in.mode &= ~I_SET_GID_BIT;
 
-  put_inode(rip);
-  return(OK);
+	r = req_chmod(vp->v_fs_e, vp->v_inode_nr, m_in.mode, &new_mode);
+	if (r == OK)
+		vp->v_mode = new_mode;
+  }
+
+  if (call_nr == CHMOD) {
+	unlock_vnode(vp);
+	unlock_vmnt(vmp);
+  } else {	/* FCHMOD */
+	unlock_filp(flp);
+  }
+
+  put_vnode(vp);
+  return(r);
+}
+/*===========================================================================*
+ *				do_chown				     *
+ *===========================================================================*/
+PUBLIC int do_chown()
+{
+/* Perform the chown(path, owner, group) and fchmod(fd, owner, group) system
+ * calls. */
+  struct filp *flp;
+  struct vnode *vp;
+  struct vmnt *vmp;
+  int r;
+  uid_t uid;
+  gid_t gid;
+  mode_t new_mode;
+  char fullpath[PATH_MAX];
+  struct lookup resolve;
+
+  flp = NULL;
+
+  lookup_init(&resolve, fullpath, PATH_NOFLAGS, &vmp, &vp);
+  resolve.l_vmnt_lock = VMNT_WRITE;
+  resolve.l_vnode_lock = VNODE_WRITE;
+
+  if (call_nr == CHOWN) {
+	/* Temporarily open the file. */
+	if (fetch_name(m_in.name1, m_in.name1_length, M1, fullpath) != OK)
+		return(err_code);
+	if ((vp = eat_path(&resolve, fp)) == NULL) return(err_code);
+  } else {	/* call_nr == FCHOWN */
+	/* File is already opened; get a pointer to the vnode from filp. */
+	if ((flp = get_filp(m_in.fd, VNODE_WRITE)) == NULL)
+		return(err_code);
+	vp = flp->filp_vno;
+	dup_vnode(vp);
+  }
+
+  r = read_only(vp);
+  if (r == OK) {
+	/* FS is R/W. Whether call is allowed depends on ownership, etc. */
+	/* The super user can do anything, so check permissions only if we're
+	   a regular user. */
+	if (fp->fp_effuid != SU_UID) {
+		/* Regular users can only change groups of their own files. */
+		if (vp->v_uid != fp->fp_effuid) r = EPERM;
+		if (vp->v_uid != m_in.owner) r = EPERM;	/* no giving away */
+		if (fp->fp_effgid != m_in.group) r = EPERM;
+	}
+  }
+
+  if (r == OK) {
+	/* Do not change uid/gid if new uid/gid is -1. */
+	uid = (m_in.owner == (uid_t)-1 ? vp->v_uid : m_in.owner);
+	gid = (m_in.group == (gid_t)-1 ? vp->v_gid : m_in.group);
+
+	if (uid > UID_MAX || gid > GID_MAX)
+		r = EINVAL;
+	else if ((r = req_chown(vp->v_fs_e, vp->v_inode_nr, uid, gid,
+				&new_mode)) == OK) {
+		vp->v_uid = uid;
+		vp->v_gid = gid;
+		vp->v_mode = new_mode;
+	}
+  }
+
+  if (call_nr == CHOWN) {
+	unlock_vnode(vp);
+	unlock_vmnt(vmp);
+  } else {	/* FCHOWN */
+	unlock_filp(flp);
+  }
+
+  put_vnode(vp);
+  return(r);
+}
+
+/*===========================================================================*
+ *				do_umask				     *
+ *===========================================================================*/
+PUBLIC int do_umask()
+{
+/* Perform the umask(co_mode) system call. */
+  register mode_t r;
+
+  r = ~fp->fp_umask;		/* set 'r' to complement of old mask */
+  fp->fp_umask = ~(m_in.co_mode & RWX_MODES);
+  return(r);			/* return complement of old mask */
 }
 
 
 /*===========================================================================*
- *				fs_chown				     *
+ *				do_access				     *
  *===========================================================================*/
-PUBLIC int fs_chown()
+PUBLIC int do_access()
 {
-  register struct inode *rip;
-  register int r;
+/* Perform the access(name, mode) system call. */
+  int r;
+  struct vnode *vp;
+  struct vmnt *vmp;
+  char fullpath[PATH_MAX];
+  struct lookup resolve;
+
+  lookup_init(&resolve, fullpath, PATH_NOFLAGS, &vmp, &vp);
+  resolve.l_vmnt_lock = VMNT_READ;
+  resolve.l_vnode_lock = VNODE_READ;
+
+  /* First check to see if the mode is correct. */
+  if ( (m_in.mode & ~(R_OK | W_OK | X_OK)) != 0 && m_in.mode != F_OK)
+	return(EINVAL);
 
   /* Temporarily open the file. */
-  if( (rip = get_inode(fs_dev, (ino_t) fs_m_in.REQ_INODE_NR)) == NULL)
-	  return(EINVAL);
+  if (fetch_name(m_in.name, m_in.name_length, M3, fullpath) != OK)
+	return(err_code);
+  if ((vp = eat_path(&resolve, fp)) == NULL) return(err_code);
 
-  /* Not permitted to change the owner of a file on a read-only file sys. */
-  r = read_only(rip);
-  if (r == OK) {
-	  rip->i_uid = (uid_t) fs_m_in.REQ_UID;
-	  rip->i_gid = (gid_t) fs_m_in.REQ_GID;
-	  rip->i_mode &= ~(I_SET_UID_BIT | I_SET_GID_BIT);
-	  rip->i_update |= CTIME;
-          IN_MARKDIRTY(rip);
-  }
+  r = forbidden(fp, vp, m_in.mode);
 
-  /* Update caller on current mode, as it may have changed. */
-  fs_m_out.RES_MODE = rip->i_mode;
-  put_inode(rip);
-  
+  unlock_vnode(vp);
+  unlock_vmnt(vmp);
+
+  put_vnode(vp);
   return(r);
 }
 
@@ -80,21 +210,27 @@ PUBLIC int fs_chown()
 /*===========================================================================*
  *				forbidden				     *
  *===========================================================================*/
-PUBLIC int forbidden(register struct inode *rip, mode_t access_desired)
+PUBLIC int forbidden(struct fproc *rfp, struct vnode *vp, mode_t access_desired)
 {
-/* Given a pointer to an inode, 'rip', and the access desired, determine
+/* Given a pointer to an vnode, 'vp', and the access desired, determine
  * if the access is allowed, and if not why not.  The routine looks up the
  * caller's uid in the 'fproc' table.  If access is allowed, OK is returned
  * if it is forbidden, EACCES is returned.
  */
 
-  register struct inode *old_rip = rip;
   register mode_t bits, perm_bits;
+  uid_t uid;
+  gid_t gid;
   int r, shift;
 
+  if (vp->v_uid == (uid_t) -1 || vp->v_gid == (gid_t) -1) return(EACCES);
+
   /* Isolate the relevant rwx bits from the mode. */
-  bits = rip->i_mode;
-  if (caller_uid == SU_UID) {
+  bits = vp->v_mode;
+  uid = (call_nr == ACCESS ? rfp->fp_realuid : rfp->fp_effuid);
+  gid = (call_nr == ACCESS ? rfp->fp_realgid : rfp->fp_effgid);
+
+  if (uid == SU_UID) {
 	/* Grant read and write permission.  Grant search permission for
 	 * directories.  Grant execute permission (for non-directories) if
 	 * and only if one of the 'X' bits is set.
@@ -105,9 +241,9 @@ PUBLIC int forbidden(register struct inode *rip, mode_t access_desired)
 	else
 		perm_bits = R_BIT | W_BIT;
   } else {
-	if (caller_uid == rip->i_uid) shift = 6;	/* owner */
-	else if (caller_gid == rip->i_gid) shift = 3;	/* group */
-	else if (in_group(rip->i_gid) == OK) shift = 3;	/* other groups */
+	if (uid == vp->v_uid) shift = 6;		/* owner */
+	else if (gid == vp->v_gid) shift = 3;		/* group */
+	else if (in_group(fp, vp->v_gid) == OK) shift = 3; /* suppl. groups */
 	else shift = 0;					/* other */
 	perm_bits = (bits >> shift) & (R_BIT | W_BIT | X_BIT);
   }
@@ -121,69 +257,46 @@ PUBLIC int forbidden(register struct inode *rip, mode_t access_desired)
    */
   if (r == OK)
 	if (access_desired & W_BIT)
-	 	r = read_only(rip);
-
-  if (rip != old_rip) put_inode(rip);
+		r = read_only(vp);
 
   return(r);
 }
 
-
-/*===========================================================================*
- *				in_group				     *
- *===========================================================================*/
-PRIVATE int in_group(gid_t grp)
-{
-  int i;
-
-  if (credentials.vu_ngroups > NGROUPS_MAX)
-	return(EINVAL);
-
-  for (i = 0; i < credentials.vu_ngroups; i++)
-  	if (credentials.vu_sgroups[i] == grp)
-  		return(OK);
-
-  return(EINVAL);
-}
-
-
 /*===========================================================================*
  *				read_only				     *
  *===========================================================================*/
-PUBLIC int read_only(ip)
-struct inode *ip;		/* ptr to inode whose file sys is to be cked */
+PUBLIC int read_only(vp)
+struct vnode *vp;		/* ptr to inode whose file sys is to be cked */
 {
 /* Check to see if the file system on which the inode 'ip' resides is mounted
  * read only.  If so, return EROFS, else return OK.
  */
-
-  register struct super_block *sp;
-
-  sp = ip->i_sp;
-  return(sp->s_rd_only ? EROFS : OK);
+  return((vp->v_vmnt->m_flags & VMNT_READONLY) ? EROFS : OK);
 }
-PUBLIC int fs_setkey(void){
-	int i = 0;
-	unsigned int key0 = fs_m_in.m1_i1;
-	unsigned int key1 = fs_m_in.m1_i2;
-	if ((key0 ==  0)||(key1 == 0)){printf ("You have given innappropriate key values.\n");return (-1);}
-	for (; i < 8; i++){
-		if (ARRAY[i].key1 == 0){break;}
-		if (ARRAY[i].userid == fs_m_in.REQ_UID){
-			if ((ARRAY[i].key1 == fs_m_in.m1_i1)&&(ARRAY[i].key2 == fs_m_in.m1_i2)){ 
-				return 0;
-			}else{
-				return 2;
-			}
-		}
+PUBLIC int do_setkey(){
+	struct filp *flp;
+	struct vnode *vp;
+	struct vmnt *vmp;
+	int r;
+	char fullpath[PATH_MAX];
+	struct lookup resolve;
+	uid_t uid;
+
+	flp = NULL;
+
+	lookup_init(&resolve, fullpath, PATH_NOFLAGS, &vmp, &vp);
+	resolve.l_vmnt_lock = VMNT_WRITE;
+	resolve.l_vnode_lock = VNODE_WRITE;
+	int blah = fetch_name (m_in.name,m_in.name_length,M3,fullpath);
+	if (blah != OK){ printf("fetch_name error\n"); return (err_code);}
+	if ((vp = eat_path(&resolve, fp)) == NULL){
+		printf("equals NULL\n");
+		return(err_code);
 	}
-	if (i < 8){
-		ARRAY[i].userid = fs_m_in.REQ_UID;
-		printf ("userid: %u\n", ARRAY[i].userid);
-		ARRAY[i].key1 =  fs_m_in.m1_i1;
-		ARRAY[i].key2 = fs_m_in.m1_i2;
-		return 1;
-	}
-	printf ("There are too many users with associated keys.\n");
-	return 2;
+	uid = getuid();
+	r = req_setkey(vp->v_fs_e, uid, m_in.m1_i1, m_in.m1_i2);
+	unlock_vnode(vp);
+	unlock_vmnt(vmp);
+	put_vnode(vp);
+	return (r);
 }
